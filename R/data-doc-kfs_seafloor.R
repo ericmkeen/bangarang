40e6c988c34042dafcbcbdd62b5eda00c300989e
Eric Keen <eric.k@hyfe.ai>
2022-08-16 09:12:48 -0500

    adding seafloor as a dataset

---
+++ R/data-doc-kfs_seafloor.R
@@ -0,0 +1,2 @@
+#' Dataframe of seafloor data for the Kitimat Fjord System
+"kfs_seafloor"
---
+++ data-raw/seafloor.R
@@ -0,0 +1,2 @@
+kfs_seafloor <- read_csv('data-raw/depthframe.csv')
+usethis::use_data(kfs_seafloor, overwrite = TRUE)
