db710584811e108646c350f70fc025c8563f0fff
Eric Keen <eric.k@hyfe.ai>
2023-03-09 10:47:09 -0600

    added sp to dependencies and fixed gg_kfs color defaults

--- R/gg_kfs.R
+++ R/gg_kfs.R
@@ -3,6 +3,8 @@
 #' @param lat_range Two-element vector of latitudinal range
 #' @param long_range Two-element vector of longitudinal range
 #' @param land_fill Land fill color
+#' @param land_line Land border color
+#' @param land_stroke Land border thickness
 #' @param land_alpha Land fill alpha
 #' @param water_fill Water fill color
 #' @param water_alpha Water fill alpha
@@ -14,6 +16,8 @@
 gg_kfs <- function(lat_range = c(52.8, 53.55),
                    lon_range = c(-129.68, -128.85),
                    land_fill = 'darkslategrey',
+                   land_line = 'grey30',
+                   land_stroke = .25,
                    land_alpha = .2,
                    water_fill = 'lightblue',
                    water_alpha = .1){
@@ -22,6 +26,7 @@ gg_kfs <- function(lat_range = c(52.8, 53.55),
     lat_range = c(52.8, 53.55)
     lon_range = c(-129.68, -128.85)
     land_fill = 'darkslategrey'
+    land_line = 'grey30'
     land_alpha = .2
     water_fill = 'lightblue'
     water_alpha = .1
@@ -30,15 +35,14 @@ gg_kfs <- function(lat_range = c(52.8, 53.55),
   data(nepac_shore_sf)
   
   ggplot2::ggplot(nepac_shore_sf) +
-    ggplot2::geom_sf(fill='darkslategrey',
+    ggplot2::geom_sf(fill=land_fill,
                      alpha=land_alpha,
-                     color='grey30', 
-                     lwd=.25) +
+                     color=land_line, 
+                     lwd=land_stroke) +
     ggplot2::coord_sf(ylim=lat_range,
                       xlim=lon_range) + 
     ggplot2::theme_light() + 
     theme(panel.background = element_rect(fill = adjustcolor(water_fill, alpha.f=water_alpha),
-                                          #colour = "lightblue",
                                           size = 0.5, linetype = "solid"))
   
 }
